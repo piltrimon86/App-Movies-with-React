dc1c6d9878ca66166856f6df620ecb64fc5a9652
Fernando Gómez <fernandogomezrecio@gmail.com>
2024-02-26 09:45:38 +0100

    Funcionalidad completa de borrar películas

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -10,6 +10,25 @@ const MovieList = ({ listState, setListState }) => {
     const getMovies = () => {
         let movies = JSON.parse(localStorage.getItem('movies'))
         setListState(movies)
+        return movies
+    }
+
+    const deleteMovie = (id) => {
+        // Conseguir películas almacenadas
+        let storedMovies = getMovies()
+
+        // Filtrar esas películas para que elimine del array la que quiero borrar
+        let newMovieList = storedMovies.filter(
+            (movie) => movie.id !== parseInt(id)
+        )
+
+        // Actualizar el estado del listado
+
+        setListState(newMovieList)
+
+        // Actualizar los datos en el LocalStorage
+
+        localStorage.setItem('movies', JSON.stringify(newMovieList))
     }
 
     return (
@@ -21,7 +40,11 @@ const MovieList = ({ listState, setListState }) => {
                             <h3 className="title">{movie.title}</h3>
                             <p className="description">{movie.description}</p>
                             <button className="edit">Editar</button>
-                            <button className="delete">Borrar</button>
+                            <button
+                                className="delete"
+                                onClick={() => deleteMovie(movie.id)}>
+                                Borrar
+                            </button>
                         </article>
                     )
                 })

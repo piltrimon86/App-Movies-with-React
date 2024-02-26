f6d40f9e072bcddf152b6a2896804d42ef68c129
Fernando Gómez <fernandogomezrecio@gmail.com>
2024-02-26 11:35:08 +0100

    Mensaje de error cuando no localiza ninguna película en el buscador

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const MovieSearch = ({ listState, setListState }) => {
     const [search, setSearch] = useState('')
+    const [notFound, setNotFound] = useState(false)
 
     const searchMovie = (e) => {
         // Crear estado y actualizarlo
@@ -16,6 +17,9 @@ const MovieSearch = ({ listState, setListState }) => {
 
         if (search.length <= 1 || moviesSearch <= 0) {
             moviesSearch = JSON.parse(localStorage.getItem('movies'))
+            setNotFound(true)
+        } else {
+            setNotFound(false)
         }
 
         //Actualizar el listado principal con lo que he filtrado
@@ -25,6 +29,11 @@ const MovieSearch = ({ listState, setListState }) => {
     return (
         <div className="search">
             <h3 className="title">Buscador: {search}</h3>
+            {notFound === true && search.length > 1 && (
+                <span className="notFound">
+                    No se ha encontrado ninguna película
+                </span>
+            )}
             <form>
                 <input
                     type="text"
